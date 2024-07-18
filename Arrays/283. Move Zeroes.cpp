560a2d7f93e7ce3211060d7e3a42785d6ded3ce8
Sivasanath06 <msivasanathkumar@gmail.com>
2024-07-18 16:17:33 +0530

    Done

---
+++ Arrays/283. Move Zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                ans.push_back(nums[i]);
+            }
+        }
+        nums.clear();
+        for(int i=0;i<n;i++){
+            nums.push_back(ans[i]);
+        }
+    }
+};
